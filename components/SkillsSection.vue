332fa6166dbdba365125cbf309fca80c8e53efd5
Fábio Lima <limavfabio@gmail.com>
2023-07-18 14:50:45 -0300

    Clean skills section component

--- components/SkillsSection.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <div class="mt-20">
-    <TextSection title="Skills" body="I'm a Full-stack developer focused on web development with Ruby on Rails and React with TypeScript." />
-  </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,19 @@
   <div class="mx-auto px-4 container">
     <HeroSection />
 
-    <!-- About section is defined here simply with this component -->
+    <!-- About Section -->
     <TextSection
       title="About" body="Hello I’m a software developer! I can help you build a product,
       feature or website Look through some of my work and experience!
       If you like what you see and have a project you need coded, don’t hestiate to contact me."
     />
 
-    <SkillsSection />
+    <!-- Skills Section -->
+    <TextSection
+      title="Skills"
+      body="I'm a Full-stack developer focused on web development with
+     Ruby on Rails and React with TypeScript."
+    />
 
     <WorksSection />
   </div>
